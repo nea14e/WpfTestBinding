72a93b0e4f45cd97c533c21bd1712aa79fb9617b
Мы-прогер <example@mail.ru>
2026-04-27 16:09:22 +0300

    PrintPerson2 bug

---
+++ TestBinding/PersonControl.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TestBinding;
+
+public partial class PersonControl : UserControl
+{
+    public static readonly DependencyProperty PersonProperty =
+        DependencyProperty.Register("Person", typeof(Person), typeof(PersonControl));
+
+    public PersonControl()
+    {
+        PrintPerson2 = new(() =>
+        {
+            MessageBox.Show($"Person-2: {Person}");
+        });
+        InitializeComponent();
+    }
+
+    public Person Person
+    {
+        get => (Person)GetValue(PersonProperty);
+        set => SetValue(PersonProperty, value);
+    }
+
+    public DelegateCommand PrintPerson2
+    {
+        get;
+    }
+}
