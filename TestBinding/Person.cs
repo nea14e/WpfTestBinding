f145b43b5f1e8529c089d6bb6382da58c775a67c
Мы-прогер <example@mail.ru>
2026-04-27 17:07:47 +0300

    Заменил иммутабельный record на class - привязка заработала

--- TestBinding/Person.cs
+++ TestBinding/Person.cs
@@ -1,3 +1,22 @@
 ﻿namespace TestBinding;
 
-public record Person(string Name, int Number);
+public class Person
+{
+    public string Name { get; set; }
+    public int Number { get; set; }
+
+    public Person() : this("Qwerty", 123)
+    {
+    }
+
+    public Person(string name, int number)
+    {
+        Name = name;
+        Number = number;
+    }
+
+    public override string ToString()
+    {
+        return $"'{Name}', {Number}";
+    }
+}
